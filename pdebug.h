468cf1374dc12009b36aae77950a385f3a5de6e0
yi-jongwoo <yijw0930@naver.com>
2023-06-14 05:20:21 -0400

    fin

--- pdebug.h
+++ pdebug.h
@@ -1,10 +1,11 @@
-void hexdump(char* data,int len){
+int hexdump(char* data,int len){
 	std::cout<<"[start]";
 	for(int i=0;i<len;i++){
 		if(i%16==0)
-			std::cout<<endl;
+			std::cout<<std::endl;
 		std::cout<<std::hex<<(data[i]>>4&0xf)<<(data[i]&0xf)<<' '<<std::dec;
 	}
 	std::cout<<std::endl<<"[end]"<<std::endl;
+	return 0;
 }
 #define pcap_sendpacket(a,b,c) hexdump((char*)b,c)
